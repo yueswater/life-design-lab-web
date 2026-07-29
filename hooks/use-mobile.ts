9fbe4e93e934693fb9ce3b3109a366b4ae1edc8d
Anthony Sung <sungpinyue@gmail.com>
2026-07-29 13:57:50 +0800

    chore: add shadcn component library

---
+++ hooks/use-mobile.ts
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+const MOBILE_BREAKPOINT = 768
+
+function subscribe(onStoreChange: () => void) {
+  const mediaQuery = window.matchMedia(
+    `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
+  )
+  mediaQuery.addEventListener("change", onStoreChange)
+
+  return () => mediaQuery.removeEventListener("change", onStoreChange)
+}
+
+function getSnapshot() {
+  return window.innerWidth < MOBILE_BREAKPOINT
+}
+
+export function useIsMobile() {
+  return React.useSyncExternalStore(subscribe, getSnapshot, () => false)
+}
